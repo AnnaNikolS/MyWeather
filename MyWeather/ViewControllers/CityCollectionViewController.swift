87bb8b4b6325d1f9e34ff3ba7e697273ad1b24e1
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-04-11 02:52:37 +0300

    Data transfer to MoreCityVC

--- MyWeather/ViewControllers/CityCollectionViewController.swift
+++ MyWeather/ViewControllers/CityCollectionViewController.swift
@@ -8,82 +8,56 @@
 import UIKit
 
 final class CityCollectionViewController: UICollectionViewController {
-  
-    var cityLink = [
-        "https://api.openweathermap.org/data/2.5/weather?lat=55.755864&lon=37.617698&units=metric&lang=ru&appid=826e886dca4cc78c7a790f83d2366f70",
-        
-        "https:api.openweathermap.org/data/2.5/weather?lat=43.585472&lon=39.723098&units=metric&lang=ru&appid=2facd6704941d5124ba835d8dc90db12",
-        
-        "https://api.openweathermap.org/data/2.5/weather?lat=59.938784&lon=30.314997&units=metric&lang=ru&appid=2facd6704941d5124ba835d8dc90db12",
-        
-        "https://api.openweathermap.org/data/2.5/weather?lat=45.035470&lon=38.975313&units=metric&lang=ru&appid=826e886dca4cc78c7a790f83d2366f70"
-        
-    ]
     
-    //    enum CityLink: CaseIterable {
-    //        case moscow
-    //        case sochi
-    //        case piter
-    //        case krasnodar
-    //
-    //        var url: URL {
-    //            switch self {
-    //            case .moscow:
-    //                return URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=55.755864&lon=37.617698&units=metric&lang=ru&appid=826e886dca4cc78c7a790f83d2366f70")!
-    //            case .sochi:
-    //                return URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=43.585472&lon=39.723098&units=metric&lang=ru&appid=2facd6704941d5124ba835d8dc90db12")!
-    //            case .piter:
-    //                return URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=59.938784&lon=30.314997&units=metric&lang=ru&appid=2facd6704941d5124ba835d8dc90db12")!
-    //            case .krasnodar:
-    //                return URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=45.035470&lon=38.975313&units=metric&lang=ru&appid=826e886dca4cc78c7a790f83d2366f70")!
-    //            }
-    //        }
-    //    }
-    //
-    //
+    // MARK: - Private Properties
     private var weatherData = [City]()
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.putGradientCollectionVC()
         collectionView.backgroundColor = .clear
-        NetworkManager.shared.fetchWeather(for: cityLink) { models in
-            self.weatherData = models
-            self.collectionView.reloadData()
-        }
+        fetchData()
     }
     
+    // MARK: - UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         weatherData.count
-        
     }
-
+    
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cityCell", for: indexPath)
         
         guard let cell = cell as? CityCell else { return UICollectionViewCell() }
+        
         let weatherData = weatherData[indexPath.item]
+        
+        let maxTemp = Int(weatherData.main.tempMax.rounded())
+        let minTemp = Int(weatherData.main.tempMin.rounded())
         
         cell.cityNameLabel.text = weatherData.name
         if weatherData.name == "Новая Голландия" {
             cell.cityNameLabel.text = "Санкт-Петербург"
         }
         cell.cityDescriptionLabel.text = weatherData.weather.map { $0.description }.formatted()
-        let maxTemp = Int(weatherData.main.tempMax.rounded())
-        let minTemp = Int(weatherData.main.tempMin.rounded())
         cell.cityMinMaxTemperatureLabel.text = "Макс.: \(maxTemp)°, мин.: \(minTemp)°"
         cell.cityTemperatureLabel.text = String(Int(weatherData.main.temp.rounded())) + "°"
         
         cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.15)
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4).cgColor
-        
+        cell.layer.borderWidth = 0.75
+        cell.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3).cgColor
         cell.layer.cornerRadius = 20
+        
         return cell
     }
     
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? MoreCityCollectionViewController, let indexPath = collectionView!.indexPathsForSelectedItems?.first {
+            destination.cities = weatherData[indexPath.item]
+        }
+    }
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
@@ -97,6 +71,21 @@ extension CityCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         15
+    }
+}
+
+// MARK: - Networking 
+extension CityCollectionViewController {
+    func fetchData() {
+        NetworkManager.shared.fetchWeather(for: cityLink) { models in
+            switch models {
+            case .success(let weatherCities):
+                self.weatherData = weatherCities
+                self.collectionView.reloadData()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
--- MyWeather/ViewControllers/MoreCityCollectionViewController.swift
+++ MyWeather/ViewControllers/MoreCityCollectionViewController.swift
@@ -8,34 +8,72 @@
 import UIKit
 
 final class MoreCityCollectionViewController: UICollectionViewController {
-
+    
+    // MARK: - Public Properties
+    var cities: City?
+    
+    // MARK: - Private Properties
+    private var weatherData: [String] = []
+    private var isDataLoaded = false
+    private let weatherTitles = WeatherTitle.allCases
+    private let weatherItems = WeatherItem.allCases
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .clear
         view.putGradientCollectionVC()
     }
-
-    // MARK: UICollectionViewDataSource
-
+    
+    // MARK: - UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-       10
+        weatherTitles.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "moreCityCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "moreCityCell", for: indexPath)
         
         guard let cell = cell as? MoreCityCell else { return UICollectionViewCell() }
         
+        title = cities?.name
+        if cities?.name == "Новая Голландия" {
+            title = "Санкт-Петербург"
+        }
+        
+        let title = weatherTitles[indexPath.item]
+        let item = weatherItems[indexPath.item]
+        cell.configure(with: title, with: item)
+        
+        let sunrise = self.unixTimeToLocalTime(unixTime: Int64(cities?.sys.sunrise ?? 0))
+        let sunset = self.unixTimeToLocalTime(unixTime: Int64(cities?.sys.sunset ?? 0))
+        let speed = Int(cities?.wind.speed.rounded() ?? 0)
+        let feelsLike = Int(cities?.main.feelsLike.rounded() ?? 0)
+        let pressure = Int(( Double(cities?.main.pressure ?? 0) / 1.333).rounded())
+        self.weatherData = [String(describing: sunrise),
+                            String(describing: sunset),
+                            String(describing: "\(speed) м/с"),
+                            String(describing: "\(feelsLike)°"),
+                            String(describing: "\(cities?.main.humidity ?? 0)%"),
+                            String(describing: pressure)]
+        self.isDataLoaded = true
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+        
+        if self.isDataLoaded && self.weatherData.indices.contains(indexPath.item) {
+            cell.dataLabel.text = self.weatherData[indexPath.item]
+        } else {
+            cell.dataLabel.text = "-"
+        }
         
         cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.15)
-        cell.layer.borderWidth = 1
-        cell.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.4).cgColor
-        
+        cell.layer.borderWidth = 0.75
+        cell.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3).cgColor
         cell.layer.cornerRadius = 20
-    
+        
         return cell
     }
-
+    
 }
 // MARK: - UICollectionViewDelegateFlowLayout
 extension MoreCityCollectionViewController: UICollectionViewDelegateFlowLayout {
